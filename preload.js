27f945fa0e3ded290b2327519d6db3e8844afebb
dlctheo181234 <theo181208@gmail.com>
2026-02-18 22:12:21 +0100

    fixed local database

--- preload.js
+++ preload.js
@@ -1,37 +1,38 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-let dbUrl;
+let resolveDbUrl;
+const dbUrlPromise = new Promise(resolve => {
+  resolveDbUrl = resolve;
+});
 
 ipcRenderer.on('db-url', (event, url) => {
   console.log('Preload received db-url:', url);
-  dbUrl = url;
+  resolveDbUrl(url); // Resolve the promise when the URL is received
 });
 
 contextBridge.exposeInMainWorld('api', {
-  getDbUrl: () => dbUrl,
-  fetch: (url, options) => {
-    if (!dbUrl) {
+  getDbUrl: () => dbUrlPromise, // Return the promise for consumers who need it
+  fetch: async (url, options) => { // Make fetch async
+    const actualDbUrl = await dbUrlPromise; // Wait for the dbUrl to be set
+    if (!actualDbUrl) {
       console.error('Database URL not set!');
       return Promise.reject(new Error('Database URL not set'));
     }
-    const fullUrl = `${dbUrl}${url}`;
+    const fullUrl = `${actualDbUrl}${url}`;
     console.log(`Fetching from: ${fullUrl}`);
-    return fetch(fullUrl, options)
-      .then(res => {
-        return res.json().then(data => {
-          if (!res.ok) {
-            console.error(`HTTP Error: ${res.status} ${res.statusText}`, data);
-            // Si le serveur retourne un objet avec "erreur", utiliser ce message
-            const errorMsg = data.erreur || `HTTP ${res.status}: ${res.statusText}`;
-            throw new Error(errorMsg);
-          }
-          return data;
-        });
-      })
-      .catch(err => {
-        console.error(`Fetch error: ${err.message}`);
-        throw err;
-      });
+    try {
+      const res = await fetch(fullUrl, options);
+      const data = await res.json();
+      if (!res.ok) {
+        console.error(`HTTP Error: ${res.status} ${res.statusText}`, data);
+        const errorMsg = data.erreur || `HTTP ${res.status}: ${res.statusText}`;
+        throw new Error(errorMsg);
+      }
+      return data;
+    } catch (err) {
+      console.error(`Fetch error: ${err.message}`);
+      throw err;
+    }
   },
   on: (channel, callback) => {
     // Passer les arguments de l'événement au callback
